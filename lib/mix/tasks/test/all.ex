479d3d09b199abf5173ea38a24b0ce690898724b
ne_Sachirou <utakata.c4se@gmail.com>
2017-06-26 15:12:25 +0900

    TravisCI Elixir 1.4.1→1.4.4

--- lib/mix/tasks/test/all.ex
+++ lib/mix/tasks/test/all.ex
@@ -8,8 +8,8 @@ defmodule Mix.Tasks.Test.All do
     Mix.Task.run "dogma"
     IO.puts "+ mix dialyzer"
     Mix.Task.run "dialyzer"
-    IO.puts "+ MIX_ENV=test mix test"
-    {result, 0} = System.cmd "sh", ["-c", "MIX_ENV=test mix test"]
+    IO.puts "+ MIX_ENV=test mix test --color"
+    {result, 0} = System.cmd "sh", ["-c", "MIX_ENV=test mix test --color"]
     IO.puts result
   end
 end
